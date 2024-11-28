0ba3ad28eea991864ad30bb4637f7a9e2cf1de4c
ashashlin <104874547+ashashlin@users.noreply.github.com>
2024-11-29 01:42:09 +0800

    decrease/increase product quantity when clicking -/+ buttons

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,15 @@
-export const cart = [];
+export let cart = [];
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  // localStorage.setItem('cart', JSON.stringify(cart));
+}
--- scripts/main/renderProducts.js
+++ scripts/main/renderProducts.js
@@ -1,5 +1,5 @@
 import { products } from "../../data/products.js";
-import { cart } from "../../data/cart.js";
+import { cart, removeFromCart } from "../../data/cart.js";
 import { renderCart } from "./renderCart.js";
 
 export function renderProducts() {
@@ -64,18 +64,19 @@ export function renderProducts() {
           }
         });
 
-        if (matchingItem) {
-          matchingItem.quantity++;
-        } else {
+        if (!matchingItem) {
           cart.push({
             productId,
             quantity: 1
           });
 
-          let matchingItem;
+          // localStorage.setItem('cart', JSON.stringify(cart));
+
           let quantity;
 
           cart.forEach((cartItem) => {
+            let matchingItem;
+
             if (cartItem.productId === productId) {
               matchingItem = cartItem;
               quantity = matchingItem.quantity;
@@ -84,15 +85,19 @@ export function renderProducts() {
 
           document.querySelector(`.js-atc-${productId}`)
             .innerHTML = `
-              <div class="adjust-q-icon">
+              <div class="adjust-q-icon js-decrement js-decrement-${productId}"
+              data-product-id="${productId}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
                   <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
                 </svg>
               </div>
 
-              <div>${quantity}</div>
+              <div class="js-cart-quantity-${productId}">
+                ${quantity}
+              </div>
 
-              <div class="adjust-q-icon">
+              <div class="adjust-q-icon js-increment js-increment-${productId}"
+              data-product-id="${productId}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
                   <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
                 </svg>
@@ -101,11 +106,69 @@ export function renderProducts() {
 
           document.querySelector(`.js-atc-${productId}`)
             .classList.add('added-to-cart');
+
+          renderCart();
+
+          const decrementBtn = document.querySelector(`.js-decrement-${productId}`);
+
+          decrementBtn.addEventListener('click', (e) => {
+            e.stopPropagation();
+
+            const {productId} = decrementBtn.dataset;
+
+            let matchingItem;
+
+            cart.forEach((cartItem) => {
+              if (cartItem.productId === productId) {
+                matchingItem = cartItem;
+              }
+            });
+
+            if (matchingItem.quantity > 0) {
+              matchingItem.quantity--;
+
+              if (matchingItem.quantity === 0) {
+                removeFromCart(productId);
+
+                document.querySelector(`.js-atc-${productId}`)
+                  .innerHTML = `
+                    <img src="assets/images/icon-add-to-cart.svg" alt="an icon of a shopping cart with a plus sign in it">
+                    <div>Add to Cart</div>
+                  `;
+
+                document.querySelector(`.js-atc-${productId}`)
+                  .classList.remove('added-to-cart');
+
+              } else {
+                document.querySelector(`.js-cart-quantity-${productId}`)
+                  .innerHTML = `${matchingItem.quantity}`;
+              }
+
+              renderCart();
+            }
+          });
+
+          const incrementBtn = document.querySelector(`.js-increment-${productId}`);
+
+          incrementBtn.addEventListener('click', (e) => {
+            e.stopPropagation();
+
+            let matchingItem;
+
+            cart.forEach((cartItem) => {
+              if (cartItem.productId === productId) {
+                matchingItem = cartItem;
+              }
+            });
+
+            matchingItem.quantity++;
+
+            document.querySelector(`.js-cart-quantity-${productId}`)
+              .innerHTML = `${matchingItem.quantity}`;
+
+            renderCart();
+          });
         }
-
-        // localStorage.setItem('cart', JSON.stringify(cart));
-
-        renderCart();
       });
     });
 }
